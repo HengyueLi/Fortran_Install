f2ceeebf5ce6344dfea89b3ed049ff0f2a86789a
Unknown <305069590@qq.com>
2018-03-27 16:35:48 +0900

    test

--- compile.py
+++ compile.py
@@ -3,35 +3,83 @@
 
 
 
-import os,sys
-
-Compiler = sys.argv[1]
-ComFlag  = sys.argv[2]
-
-def listdir_nohidden(path):
-    for f in os.listdir(path):
-        if not f.startswith('.'):
-            yield f
-
-
-# if t = 1 , return full path
-# if t = 0 , return file name only.
-def GetListdirBySuffix(path,suffix,t = 1):
-    r = []
-    for file in os.listdir(path):
-        if file.endswith(suffix):
-            if t==0:
-                r.append(  file )
-            elif t==1:
-                r.append(  os.path.join(path, file) )
-    return r
+import argparse,sys
+#===============================================================================
+# get interpreter
+python = sys.executable
+#===============================================================================
 
 
 
 
+#===============================================================================
+#  get arguments
+#===============================================================================
+parser = argparse.ArgumentParser()
+#--------add options
+parser.add_argument("--compiler", "-c", help="Set compiler, defualt is ifort")
+parser.add_argument("--flag", "-f", help="Set flag to compiler")
+
+# read arguments from the command line
+args = parser.parse_args()
+#----------------initiate arguments---------------
+if args.compiler:
+    print("Set compiler :  %s" % args.compiler)
+    Compiler = args.compiler
+else:
+    print("Set compiler : ifort (defult) ")
+    Compiler = 'ifort'
+
+if args.flag:
+    print("Set flag :  %s" % args.flag)
+    Cflag = args.flag
+else:
+    Cflag = " "
 
 
 
+
+
+
+
+
+Compiler  = Compiler
+ComFlag   = Cflag
+PythonArg = " ".join(sys.argv[1:])
+
+
+
+class GetListDirectory():
+    def __init__(self,dpath):
+        self.path = dpath
+
+    def GetlistdirName(self):
+        return os.listdir(self.path)
+
+    def GetlistdirNameNonehidden(self):
+        l = self.GetlistdirName()
+        r = []
+        for jc in l:
+            if not jc.startswith('.'):
+                r.append(jc)
+        return r
+
+    def GetListdirNameBySuffix(self,suffix):
+        r = []
+        l = selfl.GetlistdirName()
+        for jc in l:
+            if jc.endswith(suffix):
+                r.append(jc)
+        return r
+
+    def GetFullPath(self,NameList):
+        r = []
+        for jc in NameList:
+            r.append(os.path.join(self.path, jc))
+        return r
+
+
+Filename = os.path.basename(__file__)
 FilePath = os.path.abspath(__file__)
 InstPath = os.path.dirname(FilePath)
 ProjPath = os.path.dirname(InstPath)
@@ -42,21 +90,17 @@ DepePath = os.path.join(ProjPath,'Depe')
 
 
 
-
-SubProjList = listdir_nohidden(DepePath)
-
-
-
-
-
 #--------------------------------
 #   pre-compile dependence
+
+# get list of subproject
+SubProjList = GetListDirectory(DepePath).GetlistdirNameNonehidden()
 for pro in SubProjList:
     SubProPath = os.path.join(DepePath,pro)
-    pyfile     = os.path.join(SubProPath,'Install','compile.py')
+    pyfile     = os.path.join(SubProPath,'Install',Filename)
     #-----------------------------------------------------------
     # run python file in the sub project.
-    os.system( 'python3 '+ pyfile +" " + sys.argv[1]+ " " + sys.argv[2])
+    os.system( python + " " + pyfile +" " + PythonArg )
     #-----------------------------------------------------------
     # move ofile to current/Mods folders
     ObtainedOfile = pro + '.o'
@@ -65,23 +109,31 @@ for pro in SubProjList:
     DestiPath     = os.path.join(ModsPath,ObtainedOfile)
     os.rename( OfilePath ,  DestiPath )
     #-----------------------------------------------------------
-    # move all .mod file to main project/Mods
+    # move all '.mod' files to main project/Mods
     SubMod      = os.path.join(SubProPath,'Mods')
-    submodfiles = GetListdirBySuffix(SubMod,'.mod', t = 0 )
+    submodfiles = GetListDirectory(SubMod).GetListdirNameBySuffix('.mod')
     for jc in submodfiles:
-        os.rename(  os.path.join(SubMod,jc) ,  os.path.join(ModsPath,jc)         )
+        os.rename(  os.path.join(SubMod,jc) ,  os.path.join(ModsPath,jc)    )
 
 
 
+
+
+
+
+f = GetListDirectory(ModsPath)
+DependentOfiles = f.GetFullPath( f.GetListdirNameBySuffix('.o') )
+DependentOfilesString = " ".join(DependentOfiles)
 #--------------------------------
 #  compile
-DependentOfiles = GetListdirBySuffix(ModsPath,'.o')
-DependentOfilesString = " ".join(DependentOfiles)
 os.chdir(ProjPath)
 CompileCommand = Compiler +" -c "+ ComFlag + " -IMods "+DependentOfilesString+"  *.f90 -llapack "
-print(CompileCommand)
 os.system(CompileCommand)
-os.system('mv *.mod ./Mods')
+ProjectModsfiles = GetListDirectory(ProjPath).GetListdirNameBySuffix('.mod')
+#--------------------------------
+# move mods into folder Mods
+for jc in ProjectModsfiles:
+    os.rename( os.path.join(ProjPath,jc) , os.path.join(ModsPath,jc)       )
 #-------------------------------
 #  delete dependency.o
 for jc in DependentOfiles:
